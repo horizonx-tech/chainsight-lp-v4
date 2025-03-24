1a7a6745f629e06aff3a7ab714e8e70878334d31
yashika <mehndirattayashika5@gmail.com>
2025-03-24 19:44:18 +0530

    Initialised Project Stucture

---
+++ src/constants/theme.ts
@@ -0,0 +1,12 @@
+const theme={
+    colors:{
+        text:{
+            primary: '#FFFFFF',
+            secondary: '#FFE000',
+            tertiary: '#F8FAFC'
+        },
+        background: '#000000'
+    },
+};
+
+export default theme;
