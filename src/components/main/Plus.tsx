e774cf0d76ef13cd3289efc621d87487cacee5dd
yashika <mehndirattayashika5@gmail.com>
2025-05-01 22:04:26 +0530

    fix: crosshair position fix

---
+++ src/components/main/Plus.tsx
@@ -0,0 +1,11 @@
+
+const Plus = () => {
+  return (
+    <div className="h-[30px] w-[30px] flex relative items-center justify-center">
+        <div className="h-full border absolute z-10"></div>
+        <div className="w-full border absolute z-20"></div>
+    </div>
+  )
+}
+
+export default Plus
--- src/components/main/Sponsors.tsx
+++ src/components/main/Sponsors.tsx
@@ -1,6 +1,6 @@
-import { GoPlus } from "react-icons/go";
 import { motion } from "framer-motion";
 import { useEffect, useState, useMemo} from "react";
+import Plus from "./Plus";
 
 const Sponsors = () => {
   const baseSponsors = useMemo(
@@ -21,11 +21,11 @@ const Sponsors = () => {
   return (
     <section className="w-[100vw] relative lg:border-b-2 flex  justify-center border-[#111827] " >
     <div className="absolute w-[90vw] pointer-events-none top-0 bottom-0">
-      <div className="hidden lg:flex absolute left-0 bottom-0  transform -translate-x-1/2 translate-y-1/2 text-white text-xl md:text-2xl lg:text-3xl"  >
-        <GoPlus />
+      <div className="hidden lg:flex absolute left-0 bottom-0  transform -translate-x-[45%] translate-y-[53%] text-white text-xl md:text-2xl lg:text-3xl"  >
+        <Plus />
       </div>
-      <div className="hidden lg:flex absolute right-0 bottom-0  transform translate-x-1/2 translate-y-1/2 text-white text-xl md:text-2xl lg:text-3xl"  >
-        <GoPlus />
+      <div className="hidden lg:flex absolute right-0 bottom-0  transform translate-x-[48%] translate-y-[53%] text-white text-xl md:text-2xl lg:text-3xl"  >
+        <Plus />
       </div>
       </div>
       <div className="flex relative max-w-[90vw] overflow-hidden">
