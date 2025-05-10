14fa3c9d1a22ac947da8b8591e60d61dca3e28b2
darroze <darroze@horizonx.tech>
2025-05-10 13:42:18 +0900

    use renderToString

--- src/_default.page.server.jsx
+++ src/_default.page.server.jsx
@@ -1,46 +1,12 @@
 import React from "react";
 import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr/server";
 import { meta } from "./meta";
-import { renderToPipeableStream } from "react-dom/server";
+import { renderToString } from "react-dom/server";
 
 export const render = async (pageContext) => {
   const { Page } = pageContext;
 
-  const streamChunks = [];
-  const streamPromise = new Promise((resolve, reject) => {
-    const stream = renderToPipeableStream(<Page />, {
-      onShellReady() {
-        resolve(stream);
-      },
-      onShellError(error) {
-        reject(error);
-      },
-      onError(error) {
-        console.error("SSR error:", error);
-      }
-    });
-  });
-
-  const stream = await streamPromise;
-  const { Writable } = await import("node:stream");
-  const { Buffer } = await import("node:buffer");
-
-  await new Promise((resolve) => {
-    stream.pipe(
-      new Writable({
-        write(chunk, encoding, callback) {
-          streamChunks.push(chunk);
-          callback();
-        },
-        final(callback) {
-          resolve();
-          callback();
-        }
-      })
-    );
-  });
-
-  const viewHtml = Buffer.concat(streamChunks).toString();
+  const viewHtml = renderToString(<Page />);
 
   return escapeInject`<!DOCTYPE html>
     <html lang="en">
